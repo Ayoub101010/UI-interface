35de7e330d0be31d645885a37da13c27c7132ad2
MajdoulineZer <zerhounimajdouline18@gmail.com>
2023-05-10 11:39:54 +0100

    Create version 2 of UI

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import './styles/styles.css'
 
 import Header from './components/header';
 import Body from './components/body';
+import SquareContainer from './components/SquareContainer';
+import Button from './components/Button';
+
 
 
 const App = () => (
     <div className="hey">
         <Header/>
         <Body/>
+        <SquareContainer/>
+        
     </div>
 )
 
