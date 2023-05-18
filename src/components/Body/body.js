655387dd83f15b58bb2abf530669c9e9330bf5f2
MajdoulineZer <zerhounimajdouline18@gmail.com>
2023-05-18 10:10:33 +0100

    Removing 'Button' component, Adding new functionalities at MainPanel , Renaming certain files

--- src/components/Body/body.js
+++ src/components/Body/body.js
@@ -5,12 +5,7 @@ function Body() {
     <section>
       <h2>Phased SSU Rollout</h2>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <input
-        type="text"
-        id="Dep-Rel"
-        value="Deploying Release 1.3.6"
-        style={{ fontWeight: "bold" }}
-      />
+      <input type="text" id="Dep-Rel" value="Deploying Release 1.3.6" style={{ fontWeight: "bold" }}/>
 
       <select className="Group">
         <option value="" disabled selected>
