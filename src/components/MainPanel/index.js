181459ff35130a9cb16a81323de836856c9b316b
MajdoulineZer <zerhounimajdouline18@gmail.com>
2023-05-24 03:15:25 +0100

    Adding onCityChange function to update selected city in GeoSettings,Adding onModelChange to update selected model in ModelSettings

--- src/components/MainPanel/index.js
+++ src/components/MainPanel/index.js
@@ -9,7 +9,7 @@ import PresetGroups from '../PresetGroups/index';
 import { getAllProperties, setProperty, deleteProperty } from '../../services/policyHandler';
 
 function MainPanel() {
-  const [property, setProperty] = useState({
+  const [property, settoto] = useState({
     coverage: 1,
     coverage_seed: 0,
     filter: {
@@ -20,15 +20,35 @@ function MainPanel() {
     key: 'Test::Enable',
     ns: 'ada',
     value: '2',
+    city: [],
+    model: [],
   });
 
   const onPercentageChange = (evt) => {
     const { value } = evt.target;
     console.log('mainPanel onPercentageChange value: ' + value);
-    setProperty((prevState) => ({
+    settoto((prevState) => ({
       ...prevState,
       coverage: value / 100,
     }));
+  };
+
+  const onCityChange = (selectedCity) => { // Fonction appelée lorsqu'une nouvelle ville est sélectionnée dans "ModelSettings"
+    console.log('mainPanel onCityChange selectedCity: ' + selectedCity);
+    settoto((prevState) => ({
+      ...prevState,
+      city: selectedCity,
+
+    }));
+    console.log('City in property:', city);
+  };
+  const onModelChange = (selectedModels) => {
+    console.log('mainPanel onModelChange selectedModels:', selectedModels);
+    settoto((prevState) => ({
+      ...prevState,
+      model: selectedModels,
+    }));
+    console.log('Models in property:', property.model);
   };
 
   const onClick = () => {
@@ -46,13 +66,15 @@ function MainPanel() {
   };
 
   const coverage = property.coverage;
+  const city = property.city;
+  const model = property.model;
 
   return (
     <section>
       <div className="square-container">
         <PercentageSettings coverage={coverage * 100} onChange={onPercentageChange} />
-        <ModelSettings />
-        <GeoSettings />
+        <ModelSettings onModelChange={onModelChange} selectedModels={model}  /> 
+        <GeoSettings onCityChange={onCityChange} selectedCity={city}  />{/* Passage de la fonction de rappel onCityChange à GeoSettings */}
         <ScheduleSettings />
         <UpgradeStatistics />
         <button type='button' className='btn btn-danger' onClick={onClick}>
--- src/components/ModelSettings/index.js
+++ src/components/ModelSettings/index.js
@@ -1,31 +1,48 @@
 import React from "react";
 
-function ModelSettings() {
+function ModelSettings({ onModelChange, selectedModels }) {
+  const handleModelChange = (event) => {
+    const modelName = event.target.name;
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      onModelChange([...selectedModels, modelName]);
+    } else {
+      const updatedModels = selectedModels.filter((model) => model !== modelName);
+      onModelChange(updatedModels);
+    }
+  };
+
+  const models = [
+    { name: "Technicolor DCI", id: "model1" },
+    { name: "Skyworth MBOX", id: "model2" },
+    { name: "Cisco PDS", id: "model3" },
+  ];
+
   return (
     <section>
       <div className="square">
         <div className="rect">Included STB Models</div>
         <br />
-        <form action="/action_page.php">
-          <div className="form-check">
-            &nbsp;&nbsp;<input type="checkbox" className="form-check-input" id="check1" name="Technicolor DCI" value="something" />
-            <label className="form-check-label" htmlFor="check1">
-              Technicolor DCI
-            </label>
-          </div>
-          <br />
-          <div className="form-check">
-            &nbsp;&nbsp;<input type="checkbox" className="form-check-input" id="check2" name="Skyworth MBOX" value="something" />
-            <label className="form-check-label" htmlFor="check2">
-              Skyworth MBOX
-            </label>
-          </div>
-          <br />
-          <div className="form-check">
-            &nbsp;&nbsp;<input type="checkbox" className="form-check-input" />
-            <label className="form-check-label">Cisco PDS</label>
-          </div>
-        </form>
+        {models.map((model, index) => (
+          <React.Fragment key={model.id}>
+            <div className="form-check">
+              &nbsp;&nbsp;
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id={model.id}
+                name={model.name}
+                checked={selectedModels.includes(model.name)}
+                onChange={handleModelChange}
+              />
+              <label className="form-check-label" htmlFor={model.id}>
+                {model.name}
+              </label>
+            </div>
+            {index < models.length - 1 && <br />} {/* Ajoute un saut de ligne entre chaque modèle*/}
+          </React.Fragment>
+        ))}
       </div>
     </section>
   );
