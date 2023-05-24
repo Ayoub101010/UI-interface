ab90132dce0b97473d13840b90ee590ae09dc20b
MajdoulineZer <zerhounimajdouline18@gmail.com>
2023-05-24 03:15:25 +0100

    Adding onCityChange function to update selected city in GeoSettings,Adding onModelChange to update selected model in ModelSettings

--- src/components/GeoSettings/index.js
+++ src/components/GeoSettings/index.js
@@ -1,48 +1,52 @@
 import React from "react";
 
-function GeoSettings() {
+function GeoSettings({ selectedCity, onCityChange }) {
+  const cities = [
+    { name: "Guadalajara", id: "city1" },
+    { name: "Mexico City", id: "city2" },
+    { name: "Salamanca", id: "city3" },
+    { name: "Torreon", id: "city4" },
+    { name: "Los Mochis", id: "city5" },
+    { name: "Cancun", id: "city6" }
+  ];
+
+  const handleCityChange = (event) => {
+    const cityName = event.target.name;
+    const isChecked = event.target.checked;
+
+    if (isChecked) {
+      onCityChange([...selectedCity, cityName]);
+    } else {
+      const updatedCities = selectedCity.filter((city) => city !== cityName);
+      onCityChange(updatedCities);
+    }
+  };
+
   return (
     <section>
       <div className="square">
         <div className="rect">Included Cities</div>
         <br />
         <div className="square-wrapper">
-          <div className="form-check">
-          &nbsp;&nbsp;<input type="checkbox" className="form-check-input" id="city1" name="Guadalajara " />
-            <label className="form-check-label square-checkbox" htmlFor="city1">
-              Guadalajara 
-            </label>
-          </div><br></br>
-          <div className="form-check">
-          &nbsp;&nbsp;<input type="checkbox" className="form-check-input" id="city2" name=" Mexico City" />
-            <label className="form-check-label square-checkbox" htmlFor="city2">
-              Mexico City
-            </label>
-          </div><br></br>
-          <div className="form-check">
-          &nbsp;&nbsp;<input type="checkbox" className="form-check-input" id="city3" name="Salamanca" />
-            <label className="form-check-label square-checkbox" htmlFor="city3">
-              Salamanca
-            </label>
-          </div><br></br>
-          <div className="form-check">
-          &nbsp;&nbsp;<input type="checkbox" className="form-check-input" id="city4" name="Torreon" />
-            <label className="form-check-label square-checkbox" htmlFor="city4">
-              Torreon
-            </label>
-          </div><br></br>
-          <div className="form-check">
-          &nbsp;&nbsp;<input type="checkbox" className="form-check-input" id="city5" name="Los Mochis" />
-            <label className="form-check-label square-checkbox" htmlFor="city5">
-              Los Mochis
-            </label>
-          </div><br></br>
-          <div className="form-check">
-          &nbsp;&nbsp;<input type="checkbox" className="form-check-input" id="city6" name="Cancun" />
-            <label className="form-check-label square-checkbox" htmlFor="city6">
-              Cancun
-            </label>
-          </div>
+          {cities.map((city, index) => (
+            <React.Fragment key={city.id}>
+              <div className="form-check">
+                &nbsp;&nbsp;
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id={city.id}
+                  name={city.name}
+                  checked={selectedCity.includes(city.name)}
+                  onChange={handleCityChange}
+                />
+                <label className="form-check-label square-checkbox" htmlFor={city.id}>
+                  {city.name}
+                </label>
+              </div>
+              {index !== cities.length - 1 && <br />}
+            </React.Fragment>
+          ))}
         </div>
       </div>
     </section>
@@ -50,6 +54,3 @@ function GeoSettings() {
 }
 
 export default GeoSettings;
-
-
-
