dad572f3d6bd54a6cc0877eb9556084ff6b695fc
Ayoub101010 <118363181+Ayoub101010@users.noreply.github.com>
2023-06-05 12:50:31 +0100

    Test a sort table dynamically in the Review component by entering data into an array

--- src/components/ReviewUpdates/index.js
+++ src/components/ReviewUpdates/index.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import "./ReviewUpdates.css";
+import {
+  getAllProperties,
+  setProperty,
+  deleteProperty,
+} from "../../services/policyHandler";
 
 function ReviewUpdates() {
+  const getProps = async () => {
+    console.log("Calling getAllProporties");
+    // Appeler votre fonction generateProps pour obtenir les propriétés du backend
+    const fetchedProperties = await getAllProperties();
+
+    // const configs = generateConfigs(fetchedProperties)
+    console.log(fetchedProperties);
+    // Appeler la fonction pour récupérer les propriétés du backend au chargement du composant
+  };
+  const props = [];
+  const grouped = Object.values(
+    props.reduce((result, entry) => {
+      const key = entry.tag;
+      if (!result[key]) {
+        result[key] = [];
+      }
+      result[key].push(entry);
+      return result;
+    }, {})
+  );
+  console.log(grouped);
+  const [tableData, setTableData] = useState([
+    {
+      date: "12/08/2024",
+      version: 136,
+      percentage: 50,
+      Models: "MBOX",
+      cities: "Salamanca",
+    },
+    {
+      date: "02/03/2025",
+      version: 136,
+      percentage: 50,
+      Models: "PDS",
+      cities: "Guadalajara",
+    },
+  ]);
+  useEffect(() => {
+    getProps();
+    const sortedData = [...tableData].map((row) =>
+      Object.fromEntries(Object.entries(row).sort())
+    );
+    setTableData(sortedData);
+  }, [tableData]);
+  const tableHeaders = Object.keys(tableData[0]);
+
   return (
     <div>
       <h2 className="RSU">Review Scheduled Updates</h2>
@@ -14,16 +65,26 @@ function ReviewUpdates() {
             <th colSpan="6">Phased SSU Rollout</th>
           </tr>
           <tr>
-            <th>Dates</th>
-            <th>Version</th>
-            <th>Devices / %</th>
-            <th>STD Models</th>
-            <th>Cities</th>
-            <th>Actions</th>
+            {tableHeaders.map((header) => (
+              <th key={header}>{header}</th>
+            ))}
+            {/* // <th>Dates</th>
+            // <th>Version</th>
+            // <th>Devices / %</th>
+            // <th>STD Models</th>
+            // <th>Cities</th> */}
+            {/* <th>Actions</th> */}
           </tr>
         </thead>
         <tbody className="Tbody">
-          <tr>
+          {tableData.map((row, index) => (
+            <tr key={index}>
+              {tableHeaders.map((header) => (
+                <td key={header}>{row[header]}</td>
+              ))}
+            </tr>
+          ))}
+          {/* <tr>
             <td>2023-05-28</td>
             <td>Release 1.3.6</td>
             <td>80%</td>
@@ -31,14 +92,14 @@ function ReviewUpdates() {
             <td>
               Salamanca <a href="#"> (view)</a>
             </td>
-            <td>
-              <Button className="btnA" variant="primary">
-                Edit
-              </Button>
-              <Button className="btnB" variant="danger">
-                Delete
-              </Button>
-            </td>
+            <td> */}
+          {/* <Button className="btnA" variant="primary">
+            Edit
+          </Button>
+          <Button className="btnB" variant="danger">
+            Delete
+          </Button> */}
+          {/* </td>
           </tr>
           <tr>
             <td>2023-05-29</td>
@@ -73,7 +134,7 @@ function ReviewUpdates() {
                 Delete
               </Button>
             </td>
-          </tr>
+          </tr> */}
         </tbody>
       </Table>
     </div>
